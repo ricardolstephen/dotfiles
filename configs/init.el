15520903d1c2b31d25e2a8bb8cdb13bb2e91d13f
Ricardo L. Stephen <43506361+ricardolstephen@users.noreply.github.com>
2020-08-21 12:02:05 -0700

    .

--- configs/init.el
+++ configs/init.el
@@ -23,14 +23,15 @@
     (global-display-line-numbers-mode))
 
   ;; Enable global fci
-  (add-to-list 'package-selected-packages 'fill-column-indicator)
-  (require 'fill-column-indicator)
-  (define-global-minor-mode global-fci-mode fci-mode
-    (lambda () (when (and (not (string-match "^\*.*\*$" (buffer-name)))
-                          (not (equal major-mode 'dired-mode))
-                          (not (equal major-mode 'scratch-mode)))
-                 (fci-mode 1))))
-  (global-fci-mode)
+  (unless (version< emacs-version "25.1")
+    (add-to-list 'package-selected-packages 'fill-column-indicator)
+    (require 'fill-column-indicator)
+    (define-global-minor-mode global-fci-mode fci-mode
+      (lambda () (when (and (not (string-match "^\*.*\*$" (buffer-name)))
+                            (not (equal major-mode 'dired-mode))
+                            (not (equal major-mode 'scratch-mode)))
+                   (fci-mode 1))))
+    (global-fci-mode))
 
   ;; Disable backup files
   (setq make-backup-files nil)
